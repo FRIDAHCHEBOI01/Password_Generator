11b049db909e73cd2bfa52def36fcaff155d9942
FRIDAHCHEBOI01 <fridah.cheboi@ashesi.edu.gh>
2022-11-28 23:30:24 +0000

    short but sth:)

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,1 +1,16 @@
+import string
+import random
+numbers = string.digits
+alphabet = string.ascii_letters
+special_char = string.punctuation
+all_characters = numbers + alphabet + special_char
+
+size  = int(input('Please enter prefered length of password:)'))
+password = []
+for i in range(size):
+    password += random.choice(all_characters)
+
+
+print(''.join(password))
+
 #make use of secret module
